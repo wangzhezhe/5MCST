92ae3a34c48ac1f9b2398b0b150bfb31b2679e79
wangzhezhe <w_hessen@126.com>
2019-08-14 01:13:00 -0400

    add install example

---
+++ cmake/FooBarInstall/bar/bar.cpp
@@ -0,0 +1,8 @@
+
+#include "foo/foo.h"
+
+int main(int argc, char* argv[])
+{
+	foo();
+	return 0;
+}
---
+++ cmake/FooBarInstall/foo/foo.h
@@ -0,0 +1,10 @@
+
+#pragma once
+#ifndef FOOBAR_FOO_H
+#define FOOBAR_FOO_H
+
+#include <foo/config.h>
+
+void foo(void);
+
+#endif
